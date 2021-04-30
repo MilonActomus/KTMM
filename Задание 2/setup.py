bea8ab6c49381d94af55db1dfb7d66f806a4ba7f
MilonActomus <79524626+MilonActomus@users.noreply.github.com>
2021-04-30 21:57:08 +0300

    Delete setup.py

--- Задание 2/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(
-    ext_modules=cythonize("E:/Python/Задание2/A_cyth.pyx"), include_dirs=[numpy.get_include()]
-)
